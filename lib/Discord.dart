c746c73d00d902354c2cfb1773b37512e0b3426b
OrangeCat <no>
2021-03-10 22:21:32 +0100

    add: initial commit

---
+++ lib/Discord.dart
@@ -0,0 +1,9 @@
+export 'Webhook/Webhook.dart';
+export 'Webhook/WebhookMessage.dart';
+export 'Webhook/Embed/Embed.dart';
+export 'Webhook/Embed/EmbedAuthor.dart';
+export 'Webhook/Embed/EmbedField.dart';
+export 'Webhook/Embed/EmbedFooter.dart';
+export 'Webhook/Embed/EmbedImage.dart';
+
+// TODO: Support file upload for embeds
---
+++ lib/Webhook/Embed/Embed.dart
@@ -0,0 +1,43 @@
+import 'EmbedAuthor.dart' as discord;
+import 'EmbedField.dart' as discord;
+import 'EmbedFooter.dart' as discord;
+import 'EmbedImage.dart' as discord;
+
+class Embed {
+  final int? color;
+  final List<discord.EmbedAuthor>? author;
+  final String? title;
+  final String? url;
+  final String? description;
+  final List<discord.EmbedField>? fields;
+  final discord.EmbedImage? image;
+  final discord.EmbedImage? thunmbnail;
+  final discord.EmbedFooter? footer;
+  final DateTime? timestamp;
+
+  Embed({
+    this.color,
+    this.author,
+    this.title,
+    this.url,
+    this.description,
+    this.fields,
+    this.image,
+    this.thunmbnail,
+    this.footer,
+    this.timestamp,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'color': color,
+        'author': author,
+        'title': title,
+        'url': url,
+        'description': description,
+        'fields': fields,
+        'image': image,
+        'thunmbnail': thunmbnail,
+        'footer': footer,
+        'timestamp': timestamp?.toIso8601String(),
+      };
+}
